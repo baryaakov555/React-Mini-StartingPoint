bd4dc1c7c96bfc2b30ff834b8bc3d224c5ce1695
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2025-06-23 17:46:41 -0400

    completed final problems

--- src/app.jsx
+++ src/app.jsx
@@ -9,6 +9,7 @@ const App = () => {
     ["", "", ""],
   ]);
   const [currentColor, setCurrentColor] = useState("Red");
+  const [isMouseButtonDown, setIsMouseButtonDown] = useState(false);
 
   function addRow() {
     const tempGrid = [...grid];
@@ -45,20 +46,65 @@ const App = () => {
     setCurrentColor(event.target.value);
   }
 
+  function fillAll() {
+    const tempGrid = [...grid];
+    for (let i = 0; i < tempGrid.length; i++) {
+      for (let j = 0; j < tempGrid[i].length; j++) {
+        tempGrid[i][j] = currentColor;
+      }
+    }
+    setGrid(tempGrid);
+  }
+
+  function fillUncolored() {
+    const tempGrid = [...grid];
+    for (let i = 0; i < tempGrid.length; i++) {
+      for (let j = 0; j < tempGrid[i].length; j++) {
+        if (tempGrid[i][j] == "") {
+          tempGrid[i][j] = currentColor;
+        }
+      }
+    }
+    setGrid(tempGrid);
+  }
+
+  function clearGrid() {
+    const tempGrid = [...grid];
+    for (let i = 0; i < tempGrid.length; i++) {
+      for (let j = 0; j < tempGrid[i].length; j++) {
+        tempGrid[i][j] = "";
+      }
+    }
+    setGrid(tempGrid);
+  }
+  function updateColor(row, col) {
+    const tempGrid = [...grid];
+    tempGrid[row][col] = currentColor;
+    setGrid(tempGrid);
+  }
+
   return (
     <div className="app">
       <table>
-        <tbody>
+        <tbody draggable={false}>
           {grid.map((row, rowIndex) => (
             <tr>
               {row.map((color, columnIndex) => (
                 <td
                   style={{ backgroundColor: color }}
-                  onClick={(event) => {
-                    console.log(`ROW: ${rowIndex}\nCOLUMN:${columnIndex}`);
-                    const tempGrid = [...grid];
-                    tempGrid[rowIndex][columnIndex] = currentColor;
-                    setGrid(tempGrid);
+                  onClick={() => updateColor(rowIndex, columnIndex)}
+                  onMouseDown={() => {
+                    setIsMouseButtonDown(true);
+                    updateColor(rowIndex, columnIndex);
+                  }}
+                  onMouseUp={() => {
+                    setIsMouseButtonDown(false);
+                  }}
+                  onMouseOver={(event) => {
+                    if (isMouseButtonDown) {
+                      console.log(`ROWS: ${rowIndex}\nCOLUMNS: ${columnIndex}`);
+                      updateColor(rowIndex, columnIndex);
+                    }
                   }}
                 ></td>
               ))}
@@ -70,6 +116,9 @@ const App = () => {
       <button onClick={addColumn}>Add Column</button>
       <button onClick={removeRow}>remove Row</button>
       <button onClick={removeColumn}>remove Column</button>
+      <button onClick={fillAll}>Fill all Colors</button>
+      <button onClick={fillUncolored}>Fill Uncolored</button>
+      <button onClick={clearGrid}>Clear Grid</button>
       <select onChange={chooseColor}>
         <option value="Red">Red</option>
         <option value="Blue">Blue</option>
